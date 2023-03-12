b5cf682db8e3fff69c44fff1ed39162c2c6fbcc7
Harrys Kedjnane <harrys.kedjnane@epita.fr>
2023-03-12 03:29:32 +0100

    home-manager reads name and home from env variables

--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "$USER";
-  home.homeDirectory = "$HOME";
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
