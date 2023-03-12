93595f3588136dd9c715e4e9d9eca7e6ed9bb1d1
Harrys Kedjnane <harrys.kedjnane@epita.fr>
2023-03-12 03:25:27 +0100

    replaced hardcoded name and home by env variable

--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "harsane";
-  home.homeDirectory = "/home/harsane";
+  home.username = "$USER";
+  home.homeDirectory = "$HOME";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
